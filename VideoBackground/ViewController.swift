58011f2f5a941b61c9aabd0685c529f7dc84dd00
brendon cecilio <brendon.cecilio@gmail.com>
2020-06-04 00:17:35 -0400

    imported avFoundation

--- VideoBackground/ViewController.swift
+++ VideoBackground/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playBackgroundVideo()
     }
 
-
+    private func playBackgroundVideo() {
+        
+    }
 }
 
