25c5f8fc81825ad1126aeae038d0fb37f37f7fd5
brendon cecilio <brendon.cecilio@gmail.com>
2020-06-04 00:11:58 -0400

    intial files

---
+++ VideoBackground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoBackground
+//
+//  Created by Brendon Cecilio on 6/4/20.
+//  Copyright © 2020 Brendon Cecilio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
